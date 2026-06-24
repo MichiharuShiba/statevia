d2c3589bd212a1de66e034e0db66824e1bec1e32
m-shiba <michiharu1203@ymail.ne.jp>
2026-06-25 00:32:38 +0900

    PrivateCtorActionModuleの新規作成とプロジェクトファイルの追加

---
+++ action-host/Fixtures/PrivateCtorActionModule/PrivateCtorActionModule.cs
@@ -0,0 +1,29 @@
+using Statevia.Modules;
+
+namespace Statevia.ActionHost.Tests.Fixtures.PrivateCtorActionModule;
+
+/// <summary>公開コンストラクタが無く load 時にスキップされるテスト Module。</summary>
+#pragma warning disable S3453 // テスト用: 公開コンストラクタ無しで Module インスタンス化失敗を再現する
+public sealed class PrivateCtorActionModule : IActionModule
+#pragma warning restore S3453
+{
+    private PrivateCtorActionModule()
+    {
+    }
+
+    /// <inheritdoc />
+    public string ModuleId => "private.ctor";
+
+    /// <inheritdoc />
+    public string Name => "Private Ctor Module";
+
+    /// <inheritdoc />
+    public string Version => "1.0.0";
+
+    /// <inheritdoc />
+    public IEnumerable<ModuleActionRegistration> GetActions(IServiceProvider serviceProvider)
+    {
+        _ = serviceProvider;
+        yield break;
+    }
+}
